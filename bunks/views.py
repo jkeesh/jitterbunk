debeef23d73bd61c3bd38972641adcfdd9e6cdd8
Zach Galant <zach.galant@gmail.com>
2011-08-20 17:37:58 -0700

    passed test bunks to index

--- bunks/views.py
+++ bunks/views.py
@@ -1,10 +1,17 @@
 import facebook
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request):
     """
     The main view for the site.
     @author Eric Conner
     """
-    return render_to_response("index.html")
+    return render_to_response(
+        "index.html", 
+        {
+            "bunks":[1,2,3],
+        },
+        context_instance = RequestContext(request)
+    )
