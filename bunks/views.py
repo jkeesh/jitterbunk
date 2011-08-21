447f1157ca987086d156b863db0e4c5a40a28b09
Eric Conner <econner@cs.stanford.edu>
2011-08-20 17:04:19 -0700

    Created a simple hello world index view.

--- bunks/views.py
+++ bunks/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+import facebook
+from django.http import HttpResponse
+
+def index(request):
+    """
+    The main view for the site.
+    @author Eric Conner
+    """
+    return HttpResponse("Hello World!")
