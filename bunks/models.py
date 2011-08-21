bf957e47c492ea0fd402728222213a8c33ba0e34
Eric Conner <econner@cs.stanford.edu>
2011-08-20 18:13:54 -0700

    Created bunk model.

--- bunks/models.py
+++ bunks/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Bunk(models.Model):
+    bunker = models.ForeignKey(User, related_name="bunks_sent")
+    bunkee = models.ForeignKey(User, related_name="bunks_received")
+    created_at = models.DateTimeField(auto_now_add=True)
+    seen = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return "%s bunked %s at %s" % (bunker, bunkee, created_at)
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     fbid = models.BigIntegerField(default=0, primary_key=True)
