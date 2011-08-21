a5aef4416b768891c1eb9d1a62c48f0abef78ca0
Laney Kuenzel <laney.kuenzel@gmail.com>
2011-08-20 18:11:35 -0700

    Added profile

--- bunks/views.py
+++ bunks/views.py
@@ -1,26 +1,21 @@
-# 3rd party libs
 import facebook
-
-# Django utils
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
 from django.template import RequestContext
-
-# Settings and local imports
-from django.conf import settings
+from django.shortcuts import render_to_response
 
 def index(request):
     """
     The main view for the site.
     @author Eric Conner
     """
-    # if not request.user.is_authenticated():
-    #     return render_to_response(
-    #         "login.html",
-    #         {
-    #             "facebook_app_id": settings.FACEBOOK_API_KEY
-    #         },
-    #         context_instance = RequestContext(request)
-    #     )
-    
-    return render_to_response("index.html")
+    return HttpResponse("Hello World!")
+
+
+def profile(request, id):
+
+    return render_to_response("profile.html", {
+        "viewer": request.user,
+        "id": id,
+        },
+        context_instance=RequestContext(request)
+    )
