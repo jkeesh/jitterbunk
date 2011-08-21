b1358c631ca13c0e284e5f19a4c866ac3c93c460
Jeremy Keeshin <jkeesh@gmail.com>
2011-08-20 19:17:32 -0700

    enable admin

--- settings.py
+++ settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'bunks',
     'django_extensions',              # django extensions
 )
