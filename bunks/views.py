c4fabe3f4242a4ac679e7d40b04aa57cf14f5686
Eric Conner <econner@cs.stanford.edu>
2011-08-20 17:22:36 -0700

    Ok, templates work.  Now index points to index.html.

--- bunks/views.py
+++ bunks/views.py
@@ -1,9 +1,10 @@
 import facebook
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
     """
     The main view for the site.
     @author Eric Conner
     """
-    return HttpResponse("Hello World!")
+    return render_to_response("index.html")
