f334da181d851a298fa1a93f08f2b8d916579249
Jeremy Keeshin <jkeesh@gmail.com>
2011-08-21 17:24:57 -0700

    bunking works on profile pages

--- static/js/all.js
+++ static/js/all.js
@@ -4,3 +4,4 @@ no_logging.log = function() {};
 no_logging.info = function(){};
 window.debug = DEBUG_ON ? console: no_logging;
 window.D = window.debug;
+
--- static/js/index.js
+++ static/js/index.js
@@ -39,64 +39,67 @@ $(document).ready(function() {
     var bunkee_id = null;
     var bunkee_name = null;
     
-    var input_box = $("#search-input");
+    if(window.location.pathname == "/"){
     
-    $.widget("custom.users_catcomplete", $.ui.autocomplete, {
-        _renderMenu: function(ul, items) {
-            var self = this;
-            $.each(items,
-                function(index, item) {
-                    self._renderItem(ul, item);
-                });
-            }
-    });
-        
-    $(input_box).users_catcomplete({
-        source: function(request, response) {
-            $.ajax({
-                type: "GET",
-                url: "/ajax/user_search",
-                data: {
-                    "q": request.term,
-                },
-                success: function(result) {
-                    var json = $.parseJSON(result);
-                    response(json);
+        var input_box = $("#search-input");
+    
+    
+        $.widget("custom.users_catcomplete", $.ui.autocomplete, {
+            _renderMenu: function(ul, items) {
+                var self = this;
+                $.each(items,
+                    function(index, item) {
+                        self._renderItem(ul, item);
+                    });
                 }
-            });
-        },
-        select: function(event, ui){
-            D.log(event);
-            D.log(ui);
-            $(input_box).val(ui.item.name);
-            bunkee_id = ui.item.pk;
-            bunkee_name = ui.item.name;
-            return false;
-        }
-    }).data("users_catcomplete")._renderItem = function(ul, user) {
-        return $("<li></li>")
-               .data("item.autocomplete", user)
-               .append($(""))
-               .append($("<a class='ui-menu-item'></a>")
-               .html("<img class='search-image' src='" + user.image + "' />" + 
-                     "<span class='search-result-title'>" + user.name + 
-                     "</span>"))
-               .appendTo(ul);
-    };
+        });
+        
+        $(input_box).users_catcomplete({
+            source: function(request, response) {
+                $.ajax({
+                    type: "GET",
+                    url: "/ajax/user_search",
+                    data: {
+                        "q": request.term,
+                    },
+                    success: function(result) {
+                        var json = $.parseJSON(result);
+                        response(json);
+                    }
+                });
+            },
+            select: function(event, ui){
+                D.log(event);
+                D.log(ui);
+                $(input_box).val(ui.item.name);
+                bunkee_id = ui.item.pk;
+                bunkee_name = ui.item.name;
+                return false;
+            }
+        }).data("users_catcomplete")._renderItem = function(ul, user) {
+            return $("<li></li>")
+                   .data("item.autocomplete", user)
+                   .append($(""))
+                   .append($("<a class='ui-menu-item'></a>")
+                   .html("<img class='search-image' src='" + user.image + "' />" + 
+                         "<span class='search-result-title'>" + user.name + 
+                         "</span>"))
+                   .appendTo(ul);
+        };
     
+    }
     
-    $('#bunk-button').click(function(){
-        if(!bunkee_id) return;
+    function bunk(id, name){
         $.ajax({
             type: "POST",
             url: "/ajax/bunk",
             data: {
-                bunkee: bunkee_id
+                bunkee: id
             },
             dataType: 'JSON',
             success: function(result){
                 if(result.status == 'ok'){
-                    alert('Great! You bunked ' + bunkee_name);
+                    alert('Great! You bunked ' + name);
                     $(input_box).val('');
                     window.location.reload();
                     
@@ -107,6 +110,17 @@ $(document).ready(function() {
                 D.log(result);
             }
         });
+    }
+    
+    $('#profile-bunk').click(function(){
+       D.log($('#user-name').html());
+       bunk($(this).attr('data-id'), $.trim($('#user-name').html()));
+    });
+    
+    
+    $('#bunk-button').click(function(){
+        if(!bunkee_id) return;
+        bunk(bunkee_id, bunkee_name);
     });
     
 });
